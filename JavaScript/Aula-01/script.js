0e03c368542a2b458842f13584a9537e44fab322
ArthurHOnorio7z <arthurhonorio2008@gmail.com>
2025-01-31 12:14:12 -0300

    JavaScript

---
+++ JavaScript/Aula-01/script.js
@@ -0,0 +1,25 @@
+console.log("Ola Mundo");
+
+//Exemplo de alert
+window.alert("Ola Mundo 1");
+
+//Exemplo de confirm
+    var teste = confirm("Ola Mundo 2");
+    //Exibindo no console
+    console.log(teste);
+
+//Exemplo de Prompt()
+    var testePrompt = prompt("Digite seu nome:");
+    //exibindo o nome do usuário
+    console.log(testePrompt);
+    alert(testePrompt);
+
+//Tentativa de soma
+    var numero1 = Number.parseInt(prompt("Digite o primeiro numero:"));
+    var numero2 = Number.parseInt(prompt("Digite o segundo numero"));
+    var result = (numero1+numero2);
+    alert(result)
+    console.log(result);
+    //Verificando o tipo da variavel
+    
+    console.log(typeof(result));
